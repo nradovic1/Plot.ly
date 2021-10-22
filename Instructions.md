05d2619781679e141e571d72e9b173f9d983ebb6
Novak Radovic <novak_radovic@yahoo.com>
2021-10-22 11:47:40 -0400

    Firs tof Final Pushes

--- Instructions.md
+++ Instructions.md
@@ -1,4 +1,5 @@
-# Plot.ly
+# Plot.ly Homework - Belly Button Biodiversity
+
 ![Bacteria by filterforge.com](Images/bacteria.jpg)
 
 In this assignment, you will build an interactive dashboard to explore the [Belly Button Biodiversity dataset](http://robdunnlab.com/projects/belly-button-biodiversity/), which catalogs the microbes that colonize human navels.
@@ -44,3 +45,41 @@ The dataset reveals that a small handful of microbial species (also called opera
 Additionally, you are welcome to create any layout that you would like for your dashboard. An example dashboard is shown below:
 
 ![hw](Images/hw02.png)
+
+## Advanced Challenge Assignment (Optional)
+
+The following task is advanced and therefore optional.
+
+* Adapt the Gauge Chart from <https://plot.ly/javascript/gauge-charts/> to plot the weekly washing frequency of the individual.
+
+* You will need to modify the example gauge code to account for values ranging from 0 through 9.
+
+* Update the chart whenever a new sample is selected.
+
+![Weekly Washing Frequency Gauge](Images/gauge.png)
+
+## Deployment
+
+* Deploy your app to a free static page hosting service, such as GitHub Pages. Submit the links to your deployment and your GitHub repo.
+
+* Ensure your repository has regular commits and a thorough README.md file
+
+## Hints
+
+* Use `console.log` inside of your JavaScript code to see what your data looks like at each step.
+
+* Refer to the [Plotly.js documentation](https://plot.ly/javascript/) when building the plots.
+
+## Rubric
+
+[Unit 14 Rubric - Plot.ly Homework - Belly Button Biodiversity](https://docs.google.com/document/d/1wD_hOEJELD2hifTaECfx66xlpEdJeYm3mL8q2Zoq1vo/edit?usp=sharing)
+
+- - -
+
+## References
+
+Hulcr, J. et al.(2012) _A Jungle in There: Bacteria in Belly Buttons are Highly Diverse, but Predictable_. Retrieved from: [http://robdunnlab.com/projects/belly-button-biodiversity/results-and-data/](http://robdunnlab.com/projects/belly-button-biodiversity/results-and-data/)
+
+- - -
+
+© 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,96 @@
+
+function DropMenu() {
+    d3.json('samples.json').then((data) => {
+        console.log(data);
+        
+        let dropMenu = d3.select('#selDataset');
+        let sample = data.names;
+        sample.forEach((id) => {
+            dropMenu
+            .append("option")
+            .text(id)
+            .property("value", id);      
+        });
+
+        let default_id = sample[0]
+        demo(default_id)
+        charts(default_id)
+    })
+}
+
+function charts(id) {
+    d3.json('samples.json').then((data) => {
+        let samples = data.samples
+        let filter = data.samples.filter(s => s.id.toString() === id)[0];
+        console.log(filter)
+        
+        let otu_id = []
+        let sample_value = []
+        let sample_label = []
+        for (let i=0; i<filter.otu_labels.length; i++) {
+                otu_id.push(filter.otu_ids[i]),
+                sample_value.push(filter.sample_values[i]),
+                sample_label.push(filter.otu_labels[i])
+        }
+        console.log(sample_value)
+        
+        let trace_1 = {
+            x: sample_value.slice(0,10).reverse(),
+            y: otu_id.slice(0,10).map(x => `OTU ${x}`).reverse(),
+            type: 'bar',
+            orientation: 'h',
+        };
+
+        let data_1 = [trace_1]
+
+        let layout_1 = {
+            title: 'The Top 10 OTUs in Belly Button'
+        }
+
+        Plotly.newPlot('bar', data_1, layout_1)
+
+        let trace_2 = {
+            x: otu_id,
+            y: sample_value,
+            mode: 'markers',
+            text: sample_label,
+            marker: {
+                color: otu_id,
+                size: sample_value
+              }
+
+        }
+
+        let data_2 = [trace_2]
+
+        let layout_2 = {
+            title: 'All OTUs found',
+            xaxis: { title: "OTU ID" },
+            showlegend: false,
+            
+        }
+
+        Plotly.newPlot('bubble', data_2, layout_2);
+
+        
+    });
+}
+
+function demo(id) {
+    d3.json('samples.json').then((data) => {
+        let metadata = data.metadata;
+        let demo_box = d3.select("#sample-metadata");
+        var result = metadata.filter(x => x.id.toString() === id)[0];
+        demo_box.html("");
+        Object.entries(result).forEach(([a, b]) => {
+            demo_box.append("h6").text(`${a}:${b}`);
+        })
+    })
+}
+
+function optionChanged(sample) {
+    demo(sample);
+    charts(sample)
+}
+
+DropMenu()
